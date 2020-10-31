92d2b6791b30b24d2b08093428af3ae8200034a9
dmepilot <dmepilot@yahoo.com>
2020-10-31 13:13:05 -0600

    structure and files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,15 @@
-require './config/environment'
+# require './config/environment'
 
 class ApplicationController < Sinatra::Base
+  register Sinatra::ActiveRecordExtension
+  enable :sessions
+  set :session_secret, "my_application_secret"
+  set :views, Proc.new { File.join(root, "../views/") }
 
-  configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
-  end
+  # configure do
+  #   set :public_folder, 'public'
+  #   set :views, 'app/views'
+  # end
 
   get "/" do
     erb :welcome
